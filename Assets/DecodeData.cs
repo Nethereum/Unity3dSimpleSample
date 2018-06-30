4723c2960c48ad6fa5e829f9ac14a350b625c990
Juan Blanco <juanfranblanco@hotmail.com>
2018-06-30 21:06:22 +0100

    New requests sample, Deployment, Query, Polling

--- Assets/DecodeData.cs
+++ Assets/DecodeData.cs
@@ -61,33 +61,59 @@ public class DecodeData : MonoBehaviour {
         Debug.Log("ownerAddress " + output.Owner);
     }
 
-	[Function("GiveMeTheArray", "uint256[]")]
-	public class GiveMeTheArrayFunction:ContractMessage
+    public class ArrayUint256DynamicDeployment : ContractDeploymentMessage
+    {
+        public static string BYTECODE = "608060405234801561001057600080fd5b50610127806100206000396000f300608060405260043610603e5763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416634b04bc0481146043575b600080fd5b348015604e57600080fd5b50605560a3565b60408051602080825283518183015283519192839290830191858101910280838360005b83811015608f5781810151838201526020016079565b505050509050019250505060405180910390f35b6040805160028082526060808301845292602083019080388339019050509050600181600081518110151560d357fe5b6020908102909101015280516002908290600190811060ee57fe5b60209081029091010152905600a165627a7a72305820aa3f79a3ff9e580c10090fb0e8830490f2acbd918c76c8488c40bdaf1c9180800029";
+        public ArrayUint256DynamicDeployment() : base(BYTECODE) { }
+        public ArrayUint256DynamicDeployment(string byteCode) : base(byteCode) { }
+    }
+
+    [Function("GiveMeTheArray", "uint256[]")]
+	public class GiveMeTheArrayFunction:FunctionMessage
 	{
 
 	}
 
 	[FunctionOutput]
-	public class GiveMeTheArrayOutputDTO
+	public class GiveMeTheArrayOutputDTO:IFunctionOutputDTO
 	{
 		[Parameter("uint256[]", "result", 1)]
 		public List<BigInteger> Result {get; set;}
 	}
 
+    //Sample of new features / requests
 	public IEnumerator GetArrayUInt256()
-	{
-		var contractAddress = "0xd0828aeb00e4db6813e2f330318ef94d2bba2f60";
+    { 
 		var url = "http://localhost:8545";
-		var getDataCallUnityRequest = new EthCallUnityRequest(url);
-		var functionMessage = new GiveMeTheArrayFunction ();
-		var callInput = functionMessage.CreateCallInput(contractAddress);
+        var privateKey = "0xb5b1870957d373ef0eeffecc6e4812c0fd08f554b37b233526acc331bf1544f7";
+        var account = "0x12890d2cce102216644c59daE5baed380d84830c";
+        //initialising the transaction request sender
+        var transactionRequest = new TransactionSignedUnityRequest(url, privateKey, account);
+        //deploy the contract and True indicates we want to estimate the gas
+        yield return transactionRequest.SignAndSendDeploymentContractTransaction<ArrayUint256DynamicDeployment>(true);
 
-		yield return getDataCallUnityRequest.SendRequest(callInput, Nethereum.RPC.Eth.DTOs.BlockParameter.CreateLatest());
-		var result = getDataCallUnityRequest.Result;
+        if (transactionRequest.Exception != null)
+        {
+           Debug.Log(transactionRequest.Exception.Message);
+           yield break;
+        }
 
-		var output = functionMessage.DecodeDTOTypeOutput<GiveMeTheArrayFunction,GiveMeTheArrayOutputDTO>(result);
-		Debug.Log (output.Result [0]);
-		Debug.Log (output.Result [1]);
+        
+        var transactionHash = transactionRequest.Result;
+
+        //create a poll to get the receipt when mined
+        var transactionReceiptPolling = new TransactionReceiptPollingRequest(url);
+        yield return transactionReceiptPolling.PollForReceipt(transactionHash, 2);
+        var deploymentReceipt = transactionReceiptPolling.Result;
+
+        //Query request
+        var queryRequest = new QueryUnityRequest<GiveMeTheArrayFunction,GiveMeTheArrayOutputDTO>(url, account);
+        yield return queryRequest.Query(deploymentReceipt.ContractAddress);
+
+		var dtoResult = queryRequest.Result;
+
+		Debug.Log (dtoResult.Result [0]);
+		Debug.Log (dtoResult.Result [1]);
 	}
     // Update is called once per frame
     void Update () {
